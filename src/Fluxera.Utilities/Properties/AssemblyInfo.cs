a8c50d14cc037cc7a5bea7a06b9a33f13e5a4c23
Matthias Gernand <matthias.gernand@fluxera.com>
2021-12-06 22:32:58 +0100

    Added initial repository layout.

---
+++ src/Fluxera.Utilities/Properties/AssemblyInfo.cs
@@ -0,0 +1,4 @@
+﻿using System.Reflection;
+
+[assembly: AssemblyTitle("Fluxera.Utilities")]
+[assembly: AssemblyDescription("A collection of common utilities and extension methods.")]
---
+++ src/Fluxera.Utilities/TempPathHelper.cs
@@ -0,0 +1,71 @@
+﻿namespace Fluxera.Utilities
+{
+	using System.IO;
+	using JetBrains.Annotations;
+
+	/// <summary>
+	///     A helper class for temporary files and folders.
+	/// </summary>
+	[PublicAPI]
+	public static class TempPathHelper
+	{
+		/// <summary>
+		///     Try to create a temp file up to ten times until failure.
+		/// </summary>
+		/// <param name="extension">The desired file extension. The default is <c>tmp</c>.</param>
+		/// <exception cref="IOException">When no file could be created after ten attempts.</exception>
+		/// <returns>The path of the temp file.</returns>
+		public static string CreateTempFile(string extension = "tmp")
+		{
+			int attempt = 0;
+			while(true)
+			{
+				string fileName = Path.GetRandomFileName();
+				fileName = Path.ChangeExtension(fileName, $".{extension.Trim().TrimStart('.')}");
+				fileName = Path.Combine(Path.GetTempPath(), fileName);
+
+				try
+				{
+					using FileStream fileStream = new FileStream(fileName, FileMode.CreateNew);
+					return fileName;
+				}
+				catch(IOException ex)
+				{
+					if(++attempt == 10)
+					{
+						throw new IOException("No unique temporary file name is available.", ex);
+					}
+				}
+			}
+		}
+
+		/// <summary>
+		///     Try to create a temp folger up to ten times until failure.
+		/// </summary>
+		/// <exception cref="IOException">When no file could be created after ten attempts.</exception>
+		/// <returns>The path of the temp folger.</returns>
+		public static string CreateTempFolder()
+		{
+			int attempt = 0;
+			while(true)
+			{
+				string fileName = Path.GetRandomFileName();
+				fileName = Path.Combine(Path.GetTempPath(), fileName);
+
+				try
+				{
+					Directory.CreateDirectory(fileName);
+
+					return fileName;
+				}
+				catch(IOException ex)
+				{
+					if(++attempt == 10)
+					{
+						throw new IOException("No unique temporary directory name is available.", ex);
+					}
+				}
+			}
+		}
+	}
+}
