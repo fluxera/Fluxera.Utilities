b8fa8a2c2e743b631931415b70b256c412228465
Matthias Gernand <matthias.gernand@fluxera.com>
2021-12-22 16:31:30 +0100

    Added new specialized list for types.

---
+++ src/Fluxera.Utilities/Collections/ITypeList.cs
@@ -0,0 +1,42 @@
+﻿// ReSharper disable once CheckNamespace
+
+namespace Fluxera.Collections.Generic
+{
+	using System;
+	using System.Collections.Generic;
+	using JetBrains.Annotations;
+
+	/// <summary>
+	///     A specialized <see cref="IList{T}" /> that enforces a constraint on the
+	///     base type of the types to add tio this list.
+	/// </summary>
+	/// <typeparam name="TBaseType">The base type of the types that can be added to this list.</typeparam>
+	[PublicAPI]
+	public interface ITypeList<in TBaseType> : IList<Type>
+	{
+		/// <summary>
+		///     Adds a type to the list.
+		/// </summary>
+		/// <typeparam name="T"></typeparam>
+		void Add<T>() where T : TBaseType;
+
+		/// <summary>
+		///     Adds a type to the list if it is not already in the list.
+		/// </summary>
+		/// <typeparam name="T"></typeparam>
+		bool TryAdd<T>() where T : TBaseType;
+
+		/// <summary>
+		///     Checks if the type exists in the list.
+		/// </summary>
+		/// <typeparam name="T"></typeparam>
+		/// <returns></returns>
+		bool Contains<T>() where T : TBaseType;
+
+		/// <summary>
+		///     Removes a type from the list.
+		/// </summary>
+		/// <typeparam name="T"></typeparam>
+		void Remove<T>() where T : TBaseType;
+	}
+}
---
+++ tests/Fluxera.Utilities.UnitTests/Collections/TypeListTests.cs
@@ -0,0 +1,52 @@
+﻿namespace Fluxera.Utilities.UnitTests.Collections
+{
+	using System;
+	using System.Collections.Generic;
+	using FluentAssertions;
+	using Fluxera.Collections.Generic;
+	using NUnit.Framework;
+
+	[TestFixture]
+	public class TypeListTests
+	{
+		private interface ITestInterface
+		{
+		}
+
+		private class TestClass : ITestInterface
+		{
+		}
+
+		private class NoGoodTestClass
+		{
+		}
+
+		[Test]
+		public void ShouldAllowInheritedType()
+		{
+			ITypeList<ITestInterface> list = new TypeList<ITestInterface>();
+			list.Add<TestClass>();
+
+			list[0].Should().Be<TestClass>();
+		}
+
+
+		[Test]
+		public void ShouldAllowInheritedType_DefaultAdd()
+		{
+			IList<Type> list = new TypeList<ITestInterface>();
+			list.Add(typeof(TestClass));
+
+			list[0].Should().Be<TestClass>();
+		}
+
+		[Test]
+		public void ShouldDenyInheritedType_DefaultAdd()
+		{
+			IList<Type> list = new TypeList<ITestInterface>();
+
+			Action action = () => list.Add(typeof(NoGoodTestClass));
+			action.Should().Throw<ArgumentException>();
+		}
+	}
+}
