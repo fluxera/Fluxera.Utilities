01bb253ac192e27b081df865d5f05a66ba0abd8e
Matthias Gernand <matthias.gernand@fluxera.com>
2022-06-06 15:51:07 +0200

    Extended the range of teh IsCollection extension method.

--- src/Fluxera.Utilities/Extensions/Type/IsCollection.cs
+++ src/Fluxera.Utilities/Extensions/Type/IsCollection.cs
@@ -4,6 +4,7 @@ namespace Fluxera.Utilities.Extensions
 {
 	using System;
 	using System.Collections;
+	using System.Collections.Generic;
 	using System.Reflection;
 
 	public static partial class TypeExtensions
@@ -15,7 +16,36 @@ namespace Fluxera.Utilities.Extensions
 		/// <param name="type">Type.</param>
 		public static bool IsCollection(this Type type)
 		{
-			return typeof(ICollection).GetTypeInfo().IsAssignableFrom(type);
+			if(type.IsGenericType)
+			{
+				return
+					IsAssignableFromGenericType(typeof(ICollection<>), type) ||
+					IsAssignableFromGenericType(typeof(IReadOnlyCollection<>), type);
+			}
+
+			return
+				typeof(ICollection).GetTypeInfo().IsAssignableFrom(type);
+		}
+
+		private static bool IsAssignableFromGenericType(Type genericType, Type givenType)
+		{
+			Type[] interfaceTypes = givenType.GetInterfaces();
+
+			foreach(Type it in interfaceTypes)
+			{
+				if(it.IsGenericType && it.GetGenericTypeDefinition() == genericType)
+				{
+					return true;
+				}
+			}
+
+			if(givenType.IsGenericType && givenType.GetGenericTypeDefinition() == genericType)
+			{
+				return true;
+			}
+
+			Type baseType = givenType.BaseType;
+			return baseType != null && IsAssignableFromGenericType(genericType, baseType);
 		}
 	}
 }
---
+++ tests/Fluxera.Utilities.UnitTests/Extensions/Type/IsEnumerableTests.cs
@@ -0,0 +1,43 @@
+﻿namespace Fluxera.Utilities.UnitTests.Extensions.Type
+{
+	using System;
+	using System.Collections;
+	using System.Collections.Generic;
+	using FluentAssertions;
+	using Fluxera.Utilities.Extensions;
+	using NUnit.Framework;
+
+	[TestFixture]
+	public class IsEnumerableTests
+	{
+		public static IEnumerable<object[]> TestCases()
+		{
+			yield return new object[] { typeof(IEnumerable), true };
+			yield return new object[] { typeof(IEnumerable<>), true };
+
+			yield return new object[] { typeof(IList), true };
+			yield return new object[] { typeof(IList<>), true };
+			yield return new object[] { typeof(IReadOnlyList<>), true };
+
+			yield return new object[] { typeof(ICollection), true };
+			yield return new object[] { typeof(ICollection<>), true };
+			yield return new object[] { typeof(IReadOnlyCollection<>), true };
+
+			yield return new object[] { typeof(IDictionary), true };
+			yield return new object[] { typeof(IDictionary<,>), true };
+			yield return new object[] { typeof(IReadOnlyDictionary<,>), true };
+
+			yield return new object[] { typeof(ISet<>), true };
+			yield return new object[] { typeof(IReadOnlySet<>), true };
+
+			yield return new object[] { typeof(string), true };
+		}
+
+		[Test]
+		[TestCaseSource(nameof(TestCases))]
+		public void ShouldRecognizeAsEnumerable(Type type, bool expected)
+		{
+			type.IsEnumerable().Should().Be(expected);
+		}
+	}
+}
