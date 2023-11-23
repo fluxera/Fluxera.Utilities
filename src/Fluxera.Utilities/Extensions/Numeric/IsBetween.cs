3e05711d5c8599dc82616bd14baf3db5858852f2
Matthias Gernand <matthias.gernand@fluxera.com>
2023-11-23 13:06:09 +0100

    Added support for .NET 6 and .NET 7

--- src/Fluxera.Utilities/Extensions/Numeric/IsBetween.cs
+++ src/Fluxera.Utilities/Extensions/Numeric/IsBetween.cs
@@ -9,6 +9,7 @@ namespace Fluxera.Utilities.Extensions
 	/// </summary>
 	public static partial class NumericExtensions
 	{
+#if NET7_0_OR_GREATER
 		/// <summary>
 		///     Determines if the given value is between the specified values a and b (inclusive).
 		/// </summary>
@@ -22,5 +23,64 @@ namespace Fluxera.Utilities.Extensions
 				? value >= a && value <= b
 				: value >= b && value <= a;
 		}
+#endif
+
+#if NET6_0
+		/// <summary>
+		///     Determines if the given value is between the specified values a and b (inclusive).
+		/// </summary>
+		/// <returns><c>true</c> if the given value is between the specified values a and b; otherwise, <c>false</c>.</returns>
+		/// <param name="value">Value.</param>
+		/// <param name="a">The alpha component.</param>
+		/// <param name="b">The blue component.</param>
+		public static bool IsBetween(this byte value, byte a, byte b)
+		{
+			return a < b
+				? value >= a && value <= b
+				: value >= b && value <= a;
+		}
+
+		/// <summary>
+		///     Determines if the given value is between the specified values a and b (inclusive).
+		/// </summary>
+		/// <returns><c>true</c> if the given value is between the specified values a and b; otherwise, <c>false</c>.</returns>
+		/// <param name="value">Value.</param>
+		/// <param name="a">The alpha component.</param>
+		/// <param name="b">The blue component.</param>
+		public static bool IsBetween(this short value, short a, short b)
+		{
+			return a < b
+				? value >= a && value <= b
+				: value >= b && value <= a;
+		}
+
+		/// <summary>
+		///     Determines if the given value is between the specified values a and b (inclusive).
+		/// </summary>
+		/// <returns><c>true</c> if the given value is between the specified values a and b; otherwise, <c>false</c>.</returns>
+		/// <param name="value">Value.</param>
+		/// <param name="a">The alpha component.</param>
+		/// <param name="b">The blue component.</param>
+		public static bool IsBetween(this int value, int a, int b)
+		{
+			return a < b
+				? value >= a && value <= b
+				: value >= b && value <= a;
+		}
+
+		/// <summary>
+		///     Determines if the given value is between the specified values a and b (inclusive).
+		/// </summary>
+		/// <returns><c>true</c> if the given value is between the specified values a and b; otherwise, <c>false</c>.</returns>
+		/// <param name="value">Value.</param>
+		/// <param name="a">The alpha component.</param>
+		/// <param name="b">The blue component.</param>
+		public static bool IsBetween(this long value, long a, long b)
+		{
+			return a < b
+				? value >= a && value <= b
+				: value >= b && value <= a;
+		}
+#endif
 	}
 }
--- src/Fluxera.Utilities/Extensions/Numeric/IsEven.cs
+++ src/Fluxera.Utilities/Extensions/Numeric/IsEven.cs
@@ -9,6 +9,7 @@ namespace Fluxera.Utilities.Extensions
 	/// </summary>
 	public static partial class NumericExtensions
 	{
+#if NET7_0_OR_GREATER
 		/// <summary>
 		///     Determines whether the value is even.
 		/// </summary>
@@ -18,5 +19,48 @@ namespace Fluxera.Utilities.Extensions
 		{
 			return T.IsEvenInteger(value);
 		}
+#endif
+
+#if NET6_0
+		/// <summary>
+		///     Determines whether the value is even.
+		/// </summary>
+		/// <param name="value">The value to check.</param>
+		/// <returns>True, if the value is even.</returns>
+		public static bool IsEven(this byte value)
+		{
+			return value % 2 == 0;
+		}
+
+		/// <summary>
+		///     Determines whether the value is even.
+		/// </summary>
+		/// <param name="value">The value to check.</param>
+		/// <returns>True, if the value is even.</returns>
+		public static bool IsEven(this short value)
+		{
+			return value % 2 == 0;
+		}
+
+		/// <summary>
+		///     Determines whether the value is even.
+		/// </summary>
+		/// <param name="value">The value to check.</param>
+		/// <returns>True, if the value is even.</returns>
+		public static bool IsEven(this int value)
+		{
+			return value % 2 == 0;
+		}
+
+		/// <summary>
+		///     Determines whether the value is even.
+		/// </summary>
+		/// <param name="value">The value to check.</param>
+		/// <returns>True, if the value is even.</returns>
+		public static bool IsEven(this long value)
+		{
+			return value % 2 == 0;
+		}
+#endif
 	}
 }
